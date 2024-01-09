e76025ec53405e0f667212ac4153a92b257b02e8
rtkay123 <dev@kanjala.com>
2024-01-09 20:25:22 +0200

    refactor: create db extractor fn

--- crates/api-interface/src/graphql/query/category.rs
+++ crates/api-interface/src/graphql/query/category.rs
@@ -1,8 +1,7 @@
 use api_core::{api::QueryCategories, Category};
-use api_database::Client;
 use async_graphql::{Context, Object};
 
-use crate::graphql::query::Params;
+use crate::graphql::{extract_db, query::Params};
 
 use super::{pagination::paginate, ConnectionResult};
 
@@ -14,27 +13,48 @@ impl CategoryQuery {
     async fn categories(
         &self,
         ctx: &Context<'_>,
-        #[graphql(validator(min_length = 1, max_length = 100))]
-        after: Option<String>,
-        #[graphql(validator(min_length = 1, max_length = 100))]
-        before: Option<String>,
-        #[graphql(validator(minimum = 1, maximum = 100))]
-        first: Option<i32>,
-        #[graphql(validator(minimum = 1, maximum = 100))]
-        last: Option<i32>,
+        #[graphql(validator(min_length = 1, max_length = 100))] after: Option<String>,
+        #[graphql(validator(min_length = 1, max_length = 100))] before: Option<String>,
+        #[graphql(validator(minimum = 1, maximum = 100))] first: Option<i32>,
+        #[graphql(validator(minimum = 1, maximum = 100))] last: Option<i32>,
     ) -> ConnectionResult<Category> {
         let p = Params::new(after, before, first, last)?;
 
-        let database = ctx.data::<Client>().unwrap();
+        let database = extract_db(ctx)?;
 
-        let categories = database.get_categories().await.unwrap();
+        let categories = database.get_categories().await?;
 
         paginate(categories, p, 100).await
     }
 
-    async fn category_by_id(&self, ctx: &Context<'_>, id: String) -> Option<Category> {
-        let database = ctx.data::<Client>().unwrap();
+    async fn sub_categories(
+        &self,
+        ctx: &Context<'_>,
+        #[graphql(validator(min_length = 1, max_length = 100))] parent_id: Option<String>,
+        #[graphql(validator(min_length = 1, max_length = 100))] after: Option<String>,
+        #[graphql(validator(min_length = 1, max_length = 100))] before: Option<String>,
+        #[graphql(validator(minimum = 1, maximum = 100))] first: Option<i32>,
+        #[graphql(validator(minimum = 1, maximum = 100))] last: Option<i32>,
+    ) -> ConnectionResult<Category> {
+        let p = Params::new(after, before, first, last)?;
 
-        database.get_category_by_id(&id).await.unwrap()
+        let database = extract_db(ctx)?;
+
+        let categories = database
+            .get_sub_categories(parent_id.as_deref())
+            .await
+            .unwrap();
+
+        paginate(categories, p, 100).await
+    }
+
+    async fn category_by_id(
+        &self,
+        ctx: &Context<'_>,
+        id: String,
+    ) -> async_graphql::Result<Option<Category>> {
+        let database = extract_db(ctx)?;
+
+        database.get_category_by_id(&id).await.map_err(|e| e.into())
     }
 }
