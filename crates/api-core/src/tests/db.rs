e7e8fce0fb93c7a4a48545f1a03e971ea1f6a869
rtkay123 <dev@kanjala.com>
2024-01-12 01:57:11 +0200

    tests: sample core mutations

--- crates/api-core/src/tests/db.rs
+++ crates/api-core/src/tests/db.rs
@@ -1,13 +1,98 @@
 use std::{fmt::Debug, str::FromStr};
 
 use crate::{
-    api::{CoreError, LocalQueryCategories},
+    api::{
+        CoreError, LocalMutateCategories, LocalQueryCategories, MutateCategories, QueryCategories,
+    },
     Category,
 };
 
 pub struct SampleDb;
+pub struct SampleDbSend;
 
 impl LocalQueryCategories for SampleDb {
+    async fn get_categories(&self) -> Result<impl ExactSizeIterator<Item = Category>, CoreError> {
+        Ok([].into_iter())
+    }
+
+    async fn get_sub_categories(
+        &self,
+        _id: Option<impl AsRef<str> + Send + Debug>,
+    ) -> Result<impl ExactSizeIterator<Item = Category>, CoreError> {
+        Ok([].into_iter())
+    }
+
+    async fn get_category_by_id(
+        &self,
+        id: impl AsRef<str> + Send + Debug,
+    ) -> Result<Option<Category>, CoreError> {
+        if id.as_ref().is_empty() {
+            Err(CoreError::from_str("Id cannot be empty")?)
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+impl LocalMutateCategories for SampleDb {
+    async fn create_category(&self, category: &Category) -> Result<Category, CoreError> {
+        Ok(category.to_owned())
+    }
+
+    async fn update_category(
+        &self,
+        id: impl AsRef<str> + Send + Debug,
+        data: &Category,
+    ) -> Result<Option<Category>, CoreError> {
+        if id.as_ref().is_empty() {
+            Err(CoreError::from_str("Id cannot be empty")?)
+        } else {
+            Ok(Some(data.to_owned()))
+        }
+    }
+
+    async fn delete_category(
+        &self,
+        id: impl AsRef<str> + Send + Debug,
+    ) -> Result<Option<Category>, CoreError> {
+        if id.as_ref().is_empty() {
+            Err(CoreError::from_str("Id cannot be empty")?)
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+impl MutateCategories for SampleDbSend {
+    async fn create_category(&self, category: &Category) -> Result<Category, CoreError> {
+        Ok(category.to_owned())
+    }
+
+    async fn update_category(
+        &self,
+        id: impl AsRef<str> + Send + Debug,
+        data: &Category,
+    ) -> Result<Option<Category>, CoreError> {
+        if id.as_ref().is_empty() {
+            Err(CoreError::from_str("Id cannot be empty")?)
+        } else {
+            Ok(Some(data.to_owned()))
+        }
+    }
+
+    async fn delete_category(
+        &self,
+        id: impl AsRef<str> + Send + Debug,
+    ) -> Result<Option<Category>, CoreError> {
+        if id.as_ref().is_empty() {
+            Err(CoreError::from_str("Id cannot be empty")?)
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+impl QueryCategories for SampleDbSend {
     async fn get_categories(&self) -> Result<impl ExactSizeIterator<Item = Category>, CoreError> {
         Ok([].into_iter())
     }
--- crates/api-core/src/tests/mod.rs
+++ crates/api-core/src/tests/mod.rs
@@ -2,7 +2,7 @@ mod db;
 
 use std::str::FromStr;
 
-use crate::{Category, Id, api::LocalQueryCategories};
+use crate::{tests::db::SampleDbSend, Category, Id};
 
 use self::db::SampleDb;
 
@@ -31,7 +31,10 @@ fn serialise() {
 fn deserialise_list() {
     let category = create_category();
 
-    let id = Id::from_str("category:something").expect("created id from str");
+    let mut id = Id::default();
+    assert!(id.to_string().is_empty());
+
+    id = Id::from_str("category:something").expect("created id from str");
     let category_2 = Category {
         id,
         name: "Something".into(),
@@ -55,6 +58,8 @@ fn deserialise_list() {
 
 #[tokio::test]
 async fn query_returns() {
+    use crate::api::LocalQueryCategories;
+
     let db = SampleDb.get_categories().await;
     assert!(db.is_ok());
 
@@ -71,6 +76,58 @@ async fn query_returns() {
 
     let db = SampleDb.get_category_by_id("").await;
     assert!(db.is_err());
+}
 
+#[tokio::test]
+async fn mutation_returns() {
+    use crate::api::LocalMutateCategories;
 
+    let category = create_category().unwrap();
+
+    let db = SampleDb.create_category(&category).await;
+    assert!(db.is_ok());
+
+    let db = SampleDb.update_category("some:id", &category).await;
+    assert!(db.is_ok());
+
+    let db = SampleDb.delete_category("something:else").await;
+    assert!(db.is_ok());
+}
+
+#[tokio::test]
+async fn mutation_returns_send() {
+    use crate::api::LocalMutateCategories;
+
+    let category = create_category().unwrap();
+
+    let db = SampleDbSend.create_category(&category).await;
+    assert!(db.is_ok());
+
+    let db = SampleDbSend.update_category("some:id", &category).await;
+    assert!(db.is_ok());
+
+    let db = SampleDbSend.delete_category("something:else").await;
+    assert!(db.is_ok());
+}
+
+#[tokio::test]
+async fn query_returns_send() {
+    use crate::api::QueryCategories;
+
+    let db = SampleDbSend.get_categories().await;
+    assert!(db.is_ok());
+
+    let mut id = None;
+    let db = SampleDbSend.get_sub_categories(id).await;
+    assert!(db.is_ok());
+
+    id = Some("id");
+    let db = SampleDbSend.get_sub_categories(id).await;
+    assert!(db.is_ok());
+
+    let db = SampleDbSend.get_category_by_id("id").await;
+    assert!(db.is_ok());
+
+    let db = SampleDbSend.get_category_by_id("").await;
+    assert!(db.is_err());
 }
