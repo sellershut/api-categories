0c2dff72fb5ac7ee4dc449c0766816b6057d097f
rtkay123 <dev@kanjala.com>
2024-01-15 17:49:35 +0200

    refactor: create schema builder

--- crates/api-interface/src/lib.rs
+++ crates/api-interface/src/lib.rs
@@ -1,9 +1,5 @@
 use api_database::Client;
-use async_graphql::{
-    extensions::{OpenTelemetry, Tracing},
-    Schema,
-};
-use opentelemetry::trace::Tracer;
+use async_graphql::{extensions::ExtensionFactory, Schema, SchemaBuilder};
 
 use self::graphql::{mutation::Mutation, query::Query, subscription::Subscription};
 
@@ -18,32 +14,40 @@ pub struct DatabaseCredentials<'a> {
     pub db: &'a str,
 }
 
-pub async fn create_schema<T>(
-    tracer: T,
-    database: DatabaseCredentials<'_>,
-) -> Schema<Query, Mutation, Subscription>
-where
-    T: Tracer + Send + Sync + 'static,
-    <T as Tracer>::Span: Sync + Send,
-{
-    let db_client = Client::try_new(
-        database.db_dsn,
-        database.db_user,
-        database.db_pass,
-        database.db_ns,
-        database.db,
-    )
-    .await
-    .unwrap();
-    Schema::build(
-        Query::default(),
-        Mutation::default(),
-        Subscription::default(),
-    )
-    .data(db_client)
-    .extension(Tracing)
-    .extension(OpenTelemetry::new(tracer))
-    .finish()
+pub struct ApiSchemaBuilder {
+    builder: SchemaBuilder<Query, Mutation, Subscription>,
+}
+
+impl ApiSchemaBuilder {
+    pub async fn new(database: DatabaseCredentials<'_>) -> Self {
+        let db_client = Client::try_new(
+            database.db_dsn,
+            database.db_user,
+            database.db_pass,
+            database.db_ns,
+            database.db,
+        )
+        .await
+        .unwrap();
+        Self {
+            builder: Schema::build(
+                Query::default(),
+                Mutation::default(),
+                Subscription::default(),
+            )
+            .data(db_client),
+        }
+    }
+
+    pub fn with_extension(self, extension: impl ExtensionFactory) -> Self {
+        Self {
+            builder: self.builder.extension(extension),
+        }
+    }
+
+    pub fn build(self) -> Schema<Query, Mutation, Subscription> {
+        self.builder.finish()
+    }
 }
 
 #[cfg(test)]
