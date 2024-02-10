7fed76a6a5382464fcd97801b0b24040e19de199
rtkay123 <dev@kanjala.com>
2024-02-10 14:34:43 +0200

    tests: search

--- crates/api-interface/src/tests/query.rs
+++ crates/api-interface/src/tests/query.rs
@@ -57,3 +57,60 @@ async fn gql_query_sub_categories_ok() {
 
     assert!(res.errors.is_empty());
 }
+
+#[tokio::test]
+async fn gql_search() -> Result<(), Box<dyn std::error::Error>> {
+    let schema = super::init_schema().await;
+
+    let res = schema
+        .execute(
+            r#"
+           query {
+             search(first: 2, query: "Some Text") {
+               edges{
+                 cursor
+                 node{
+                   id,
+                 }
+               },
+               pageInfo {
+                 hasNextPage,
+                 hasPreviousPage
+               }
+             }
+           }
+           "#,
+        )
+        .await;
+
+    // search client not configured
+    assert!(!res.errors.is_empty());
+
+    let res_name = schema
+        .execute(
+            r#"
+           query {
+             searchWithParentName(first: 2, query: "Some Text") {
+               edges{
+                 cursor
+                 node{
+                   category {
+                       id
+                   }
+                 }
+               },
+               pageInfo {
+                 hasNextPage,
+                 hasPreviousPage
+               }
+             }
+           }
+           "#,
+        )
+        .await;
+
+    // search client not configured
+    assert_eq!(&res.errors[0].message, &res_name.errors[0].message);
+
+    Ok(())
+}
