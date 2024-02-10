a96a6ec1c609b485c34a966313060d28f31105fc
rtkay123 <dev@kanjala.com>
2024-02-10 14:49:22 +0200

    tests: category by id

--- crates/api-interface/src/tests/query.rs
+++ crates/api-interface/src/tests/query.rs
@@ -59,14 +59,35 @@ async fn gql_query_sub_categories_ok() {
 }
 
 #[tokio::test]
-async fn gql_search() -> Result<(), Box<dyn std::error::Error>> {
+async fn gql_query_category_by_id_ok() {
     let schema = super::init_schema().await;
 
     let res = schema
         .execute(
             r#"
            query {
-             search(first: 2, query: "Some Text") {
+             categoryById(id: "018d930d-073c-73c2-b9d6-24f1461c18d3") {
+               id,
+               name
+             }
+           }
+           "#,
+        )
+        .await;
+
+    dbg!(&res.errors);
+    assert!(res.errors.is_empty());
+}
+
+#[tokio::test]
+async fn gql_search_ok() -> Result<(), Box<dyn std::error::Error>> {
+    let schema = super::init_schema().await;
+
+    let res = schema
+        .execute(
+            r#"
+           query {
+             search(last: 2, query: "Some Text") {
                edges{
                  cursor
                  node{
